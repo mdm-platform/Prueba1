79473d9f54cfbaa6e5ff6b9259ea4339381d2698
MannyDiera <mdiera.dvk@gmail.com>
2024-12-03 20:14:34 -0500

    Actualizaciones al servidor

--- servidor/server.js
+++ servidor/server.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
-console.log(process.env);
 
 const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
+app.use(express.json());
+
 const port = 3001;
-
-
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -25,12 +24,9 @@ connection.connect((err) => {
 });
 
 
-
-
-
 // para leer
 app.get('/ejercicios', (req, res) => {
-    const query = 'SELECT * FROM ejercicios_lista';        //selecciona 
+    const query = 'SELECT * FROM ejercicios_lista';        //selecciona
     connection.query(query, (error, results) => {
         if (error) {
             return res.status(500).json({ error: error.message });
@@ -39,9 +35,7 @@ app.get('/ejercicios', (req, res) => {
     });
 });
 
-
-
-// para eliminar 
+// para eliminar
 app.delete('/ejercicios/:id', (req, res) => {
     const ejercicioId = req.params.id;
     const query = 'DELETE FROM ejercicios_lista WHERE id = ?';           //cual ejercicio eliminar
@@ -53,20 +47,43 @@ app.delete('/ejercicios/:id', (req, res) => {
     });
 });
 
+// para insertar un record
+app.post('/ejercicios', async (req, res) => {
+    const { name } = req.body;
 
-//para actualizar 
-app.put('/ejercicio/:id', (req, res) => {
-    const ejercicioid = req.params.id;
-    const { id, name, created_at, updeted_at, deleted_at } = req.body;
-    const query = 'UPDATE ejercicios_lista SET id = ?, name = ?, created_at = ?, updeted_at = ?, deleted_at = ? WHERE id = ?';
-    connection.query(query, [id, name, created_at, updeted_at, deleted_at, ejercicioid], (error, results) => {
-        if (error) {
-            return res.status(500).json({ error: error.message });
-        }
+    try {
+        const insertQuery = 'INSERT INTO ejercicios_lista (name) VALUES (?)';
+        await handleDatabaseOperation(insertQuery, [name]);
+        res.json({ message: 'Ejercicio insertado correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// para actualizar un record que ya existe
+app.put('/ejercicios', async (req, res) => {
+    const { name, newName } = req.body;
+
+    try {
+        const updateQuery = 'UPDATE ejercicios_lista SET name = ? WHERE name = ?';
+        await handleDatabaseOperation(updateQuery, [newName, name]);
         res.json({ message: 'Ejercicio actualizado correctamente' });
-    });
-}); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
+// extraemos la operaciones en la base de datos
+const handleDatabaseOperation = async (query, params) => {
+    return new Promise((resolve, reject) => {
+        connection.query(query, params, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(results);
+        });
+    });
+};
 
 
 app.listen(port, () => {
