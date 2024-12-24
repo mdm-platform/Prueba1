363ab3a931eab3b269ff6864cfcda459fe5d2c8b
alondra-luna <lunalondra27@gmail.com>
2024-12-23 22:48:25 -0600

    Se agrego parte de la interfaz

--- servidor/server.js
+++ servidor/server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
-
-
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
